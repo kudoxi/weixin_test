648f9f9d912a4bd16f0fc62577095b3579e035e2
wangdalong <wangdalong@scentrealm.com>
2019-06-10 17:07:39 +0800

    路由

--- weixin/urls.py
+++ weixin/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('access_token/',views.access_token,name="access_token"),
     path('answer/',views.answer,name="answer"),
     path('code/',views.code,name="code"),
+    path('userinfo/',views.userinfo,name="userinfo"),
 
 ]
