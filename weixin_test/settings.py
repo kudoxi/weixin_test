febc2a8c043892ddde4f21da8cea46d3c0aa7dbc
wangdalong <wangdalong@scentrealm.com>
2019-06-10 10:40:57 +0800

    微信账号切换

--- weixin_test/settings.py
+++ weixin_test/settings.py
@@ -123,8 +123,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 #微信公众号
-APPID = 'wx53304857b300c82a'
-APPSECRET = '7202904ac5bad3e0fe597b5b3c309733'
+APPID = 'wx64339c4b8d370d48'#'wx53304857b300c82a'
+APPSECRET = 'd4624c36b6795d1d99dcf0547af5443d'#'7202904ac5bad3e0fe597b5b3c309733'
 
 ROOT_URL = "https://xscenic.qiweiwangguo.com"
 
