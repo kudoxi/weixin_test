b885384fb8d59f7452a9d025d0364fd8ee3e0dca
wangdalong <wangdalong@scentrealm.com>
2019-06-10 13:54:52 +0800

    微信账号切换,回复微信

--- weixin/views.py
+++ weixin/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import request,HttpResponse
 from utils.WeixinAuth import Weixin
 from mydecoration.weixinAuth import *
+from weixin_test.settings import *
 import logging
+import hashlib
+
+
 
 #https://blog.csdn.net/u013538542/article/details/86983806
 # Create your views here.
@@ -16,11 +20,29 @@ def index(req):
 
 #回复微信应答
 def answer(req):
+    logging.info("--------req:"+req.get)
     signature = req.get("signature")
     timestamp = req.get("timestamp")
+    nonce = req.get("nonce")
+    echostr = req.get("echostr")
+    token = TOKEN
+    words = [token,timestamp,nonce]
+    words.sort()
+    logging.info("--------words:"+words)
+    sha1 = hashlib.sha1()
+    map(sha1.update, words)
+    hashcode = sha1.hexdigest()
+    print("handle/GET func: hashcode, signature: ", hashcode, signature)
+    if hashcode == signature:
+        return echostr
+    else:
+        return ""
+
+
 
 
 def access_token(req):
     logger = logging.getLogger('django')
     logger.info('-------------------------test')
     return HttpResponse(2222)
+
