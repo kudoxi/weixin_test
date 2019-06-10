fc2c810675c267877affa9680bda2e8b358d2256
wangdalong <wangdalong@scentrealm.com>
2019-06-10 13:57:13 +0800

    路由answer

--- weixin/urls.py
+++ weixin/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('index/',views.index,name="index"),
     path('access_token/',views.access_token,name="access_token"),
+    path('answer/',views.answer,name="answer"),
+
 ]
