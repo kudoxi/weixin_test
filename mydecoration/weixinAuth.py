009ff3b4013a5dd3c4aea91c58504bf278232da4
wangdalong <wangdalong@scentrealm.com>
2019-06-10 16:09:10 +0800

    无法在装饰器里调用access_token获取方法

--- mydecoration/weixinAuth.py
+++ mydecoration/weixinAuth.py
@@ -13,49 +13,36 @@ def weixin_auth(func):
     def wrapper(req,*args,**kwargs):
         userid = req.session.get('userid')
         if not userid:
-            redirect_url = ROOT_URL + "/weixin_test" + req.get_full_path()
             logger = logging.getLogger('django')
-            urlResp = get_access_token(req,redirect_url)
-            refresh_token = urlResp['refresh_token']
-            access_token = urlResp['access_token']
-            expires_in = urlResp['expires_in']
-            openid = urlResp['openid']
-            now = time()
-            expires_in = now + expires_in
-            refresh_token_expires_in = now + 60 * 60 * 24 * 30
-            req.session['access_token_expires_in'] = expires_in
-            req.session['access_token'] = access_token
-            req.session['refresh_token_expires_in'] = refresh_token_expires_in
-            req.session['refresh_token'] = refresh_token
-            # redirect_url = ROOT_URL+"/weixin_test"+req.get_full_path()
-            # redirect_uri = 'https://xscenic.qiweiwangguo.com/cas_api/v1/wechat/auth'
-            # url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid={}&redirect_uri={}&response_type=code&scope={}&state=123#wechat_redirect'\
-            #     .format(APPID,redirect_url,SCOPE)
-            # logger.info('-------------------------url:'+url)
-            # code = req.GET.get("code",'')
-            # if not code:
-            #     return HttpResponseRedirect(url)
-            # else:
-            #     logger.info("---------------get code:"+code)
-            #     # 3.通过code换取网页授权access_token
-            #     curl = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid={}&secret={}&code={}&grant_type=authorization_code'\
-            #         .format(APPID,APPSECRET,code)
-            #     res = requests.get(curl)
-            #     res2 = res.text
-            #     urlResp = json.loads(res2)
-            #     logger.info("--------------get urlResp:")
-            #     logger.info(urlResp)
-            #     refresh_token = urlResp['refresh_token']
-            #     access_token = urlResp['access_token']
-            #     expires_in = urlResp['expires_in']
-            #     openid = urlResp['openid']
-            #     now = time()
-            #     expires_in = now + expires_in
-            #     refresh_token_expires_in = now + 60 * 60 * 24 * 30
-            #     req.session['access_token_expires_in'] = expires_in
-            #     req.session['access_token'] = access_token
-            #     req.session['refresh_token_expires_in'] = refresh_token_expires_in
-            #     req.session['refresh_token'] = refresh_token
+            redirect_url = ROOT_URL+"/weixin_test"+req.get_full_path()
+            redirect_uri = 'https://xscenic.qiweiwangguo.com/cas_api/v1/wechat/auth'
+            url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid={}&redirect_uri={}&response_type=code&scope={}&state=123#wechat_redirect'\
+                .format(APPID,redirect_url,SCOPE)
+            logger.info('-------------------------url:'+url)
+            code = req.GET.get("code",'')
+            if not code:
+                return HttpResponseRedirect(url)
+            else:
+                logger.info("---------------get code:"+code)
+                # 3.通过code换取网页授权access_token
+                curl = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid={}&secret={}&code={}&grant_type=authorization_code'\
+                    .format(APPID,APPSECRET,code)
+                res = requests.get(curl)
+                res2 = res.text
+                urlResp = json.loads(res2)
+                logger.info("--------------get urlResp:")
+                logger.info(urlResp)
+                refresh_token = urlResp['refresh_token']
+                access_token = urlResp['access_token']
+                expires_in = urlResp['expires_in']
+                openid = urlResp['openid']
+                now = time()
+                expires_in = now + expires_in
+                refresh_token_expires_in = now + 60 * 60 * 24 * 30
+                req.session['access_token_expires_in'] = expires_in
+                req.session['access_token'] = access_token
+                req.session['refresh_token_expires_in'] = refresh_token_expires_in
+                req.session['refresh_token'] = refresh_token
                 #获取用户信息
             userinfo_url = 'https://api.weixin.qq.com/sns/userinfo?access_token={}&openid={}&lang=zh_CN'\
                 .format(access_token,openid)
